8d10eec5e58251fa6152d326a4243a77978642a1
rollin <rowlinerollin@gmail.com>
2026-02-26 15:09:54 +0800

    Initial Commit

---
+++ suisuinian Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  suisuinian Watch App
+//
+//  Created by Rollin Luo on 2026/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ suisuinian/suisuinianApp.swift
@@ -0,0 +1,17 @@
+//
+//  suisuinianApp.swift
+//  suisuinian
+//
+//  Created by Rollin Luo on 2026/2/26.
+//
+
+import SwiftUI
+
+@main
+struct suisuinianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
